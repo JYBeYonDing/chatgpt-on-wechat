f8d0be2380fe9850bc50d11b95efc7b4a64dfb75
yangjie <jamesyoung94@163.com>
2023-03-14 10:17:39 +0800

    change:参数调整

--- bot/chatgpt/chat_gpt_bot.py
+++ bot/chatgpt/chat_gpt_bot.py
@@ -63,7 +63,7 @@ class ChatGPTBot(Bot):
             response = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo",  # 对话模型的名称
                 messages=session,
-                temperature=0.6,  # 值在[0,1]之间，越大表示回复越具有不确定性
+                temperature=0.5,  # 值在[0,1]之间，越大表示回复越具有不确定性
                 #max_tokens=4096,  # 回复最大的字符数
                 top_p=1,
                 frequency_penalty=0.0,  # [-2,2]之间，该值越大则更倾向于产生不同的内容
